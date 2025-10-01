8c97ac0e874a6f4045613b49df23fcf0a9c5de22
Ana-LauraCN <lauracastro.nas@gmail.com>
2025-10-01 20:51:12 -0300

    Disciplina (Laura)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
+const disciplinasRoutes = require('./routes/disciplina');
 app.use(cors());
 app.use(express.json());
 // TODO: Membro 1 - Importar e mapear rota de Alunos
@@ -11,6 +13,9 @@ app.use(express.json());
 // TODO: Membro 3 - Importar e mapear rota de Turmas
 
 // TODO: Membro 4 - Importar e mapear rota de Disciplinas
+
+app.use('/disciplina', disciplinasRoutes);
+
 
 // TODO: Membro 5 - Importar e mapear rota de Boletins
 
