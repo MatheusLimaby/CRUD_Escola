ecc0f7845aad41f565a3c1df237d36e69a122ad9
MatheusLimaby <matheus.l.rodrigues@iesb.edu.br>
2025-10-01 19:57:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-// TODO: Membro 1 - Importar e mapear rota de []
+// TODO: Membro 1 - Importar e mapear rota de Alunos
 
-// TODO: Membro 2 - Importar e mapear rota de []
+// TODO: Membro 2 - Importar e mapear rota de Professores
 
-// TODO: Membro 3 - Importar e mapear rota de []
+// TODO: Membro 3 - Importar e mapear rota de Turmas
 
-// TODO: Membro 4 - Importar e mapear rota de []
+// TODO: Membro 4 - Importar e mapear rota de Disciplinas
 
-// TODO: Membro 5 - Importar e mapear rota de []
+// TODO: Membro 5 - Importar e mapear rota de Boletins
 
 
 // --------------------------------------------- //
