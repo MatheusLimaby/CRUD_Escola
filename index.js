69c9fdb4cbea5bff6ee2e959472593de5d2588c0
MatheusLimaby <Matheus.l.rodigues@iesb.edu.br>
2025-10-01 17:45:41 -0300

    feat: configuração inicial do projeto

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// TODO: Membro 1 - Importar e mapear rota de []
+
+// TODO: Membro 2 - Importar e mapear rota de []
+
+// TODO: Membro 3 - Importar e mapear rota de []
+
+// TODO: Membro 4 - Importar e mapear rota de []
+
+// TODO: Membro 5 - Importar e mapear rota de []
+
+
+// --------------------------------------------- //
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
